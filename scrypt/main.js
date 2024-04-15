9fcc518fdbd64d49e2bb67dbd9054497392665ad
cavedium <shports61@gmail.com>
2024-04-15 22:19:30 +0300

    add slider function

--- scrypt/main.js
+++ scrypt/main.js
@@ -5,3 +5,12 @@ hamburger.addEventListener("click", () => {
 	hamburger.classList.toggle("active");
 	navMenu.classList.toggle("active");
 })
+
+if (document.documentElement.clientWidth < 768) {
+	var swiper = new Swiper(".mySwiper", {
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
+}
