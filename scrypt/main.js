c2b6282ccb02c018851ccbda24609befd1690477
cavedium <shports61@gmail.com>
2024-04-16 15:45:16 +0300

    update scripts

--- scrypt/main.js
+++ scrypt/main.js
@@ -7,27 +7,20 @@ hamburger.addEventListener("click", () => {
 })
 
 if (document.documentElement.clientWidth < 768) {
-	// var swiper = new Swiper(".mySwiper", {
-	// 	navigation: {
-	// 		nextEl: ".swiper-button-next",
-	// 		prevEl: ".swiper-button-prev",
-	// 	},
-	// });
-
 	var swiper = new Swiper(".mySwiper", {
 		effect: "coverflow",
 		grabCursor: true,
 		centeredSlides: true,
 		slidesPerView: "auto",
 		coverflowEffect: {
-		  rotate: 50,
-		  stretch: 0,
-		  depth: 100,
-		  modifier: 1,
-		  slideShadows: true,
+			rotate: 50,
+			stretch: 0,
+			depth: 100,
+			modifier: 1,
+			slideShadows: true,
 		},
 		pagination: {
-		  el: ".swiper-pagination",
+			el: ".swiper-pagination",
 		},
-	  });
+	});
 }
